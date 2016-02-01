b8d595eae870e62d58b4ed32576472733fb4b62e
Kenneth Centurion <kcenturion@yahoo.com>
2016-02-01 16:24:35 -0600

    killall

--- rpi/etc/init.d/nodatron.sh
+++ rpi/etc/init.d/nodatron.sh
@@ -39,6 +39,7 @@ do_start () {
 do_stop () {
     log_daemon_msg "Stopping system $DAEMON_NAME daemon"
     start-stop-daemon --stop --signal TERM --pidfile $PIDFILE
+    killall nodatron
     log_end_msg $?
 }
 
