9ca7032fa6dcbb479ae6d7f8742409904b8fcf05
Kenneth Centurion <kcenturion@yahoo.com>
2016-02-23 16:59:42 -0600

    added components

--- nodatron.sh
+++ nodatron.sh
@@ -4,16 +4,6 @@
 # chmod +x nodatron.sh
 #
 
-#start the video stream
-echo "Starting video stream..."
-if pgrep mjpg_streamer > /dev/null
-then
-  echo "mjpg_streamer already running"
-else
-  LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 15 -q 50 -x 640 -y 480" -o "output_http.so -p 8171 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
-  echo "mjpg_streamer started"
-fi
-
 #start nodatron node js
 echo "Starting nodatron..."
 sudo node /usr/local/src/node_modules/nodatron/test/test.js
