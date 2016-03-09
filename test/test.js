367dfdef0d17e2ec033019313eabdfb61c1d8eb1
Kenneth Centurion <kcenturion@yahoo.com>
2016-03-09 15:54:53 -0600

    072

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-var nodatron = require('../lib/nodatron.js');
+var nodatron = require('../lib/nodatron.js'),
+    cp       = require('child_process');
 
 var arduino = new nodatron({"device" : "/dev/ttyACM0","baud" : 9600});
 
@@ -16,33 +17,101 @@ arduino.on("connected", function(){
 
   powerButton.on('on',function(){
     powerLed.turnOn();
+    panCenter();
+
+    executeCommand("/usr/local/src/startStream.sh",function(result,msg){
+      if(result){
+        console.info(msg);
+      }
+      else{
+        console.error(msg);
+      }
+    });
   });
 
   powerButton.on('off',function(){
     powerLed.turnOff();
+    motionLed.turnOff();
+    connectivityLed.turnOff();
+    activityLed.turnOff();
+    sleepCam();
+
+    executeCommand("/usr/local/src/stopStream.sh",function(result,msg){
+      if(result){
+        console.info(msg);
+      }
+      else{
+        console.error(msg);
+      }
+    });
   });
 
   motionSensorFront.on('start',function(){
-
+    if(powerButton.isOn()){
+      motionLed.startBlink();
+      panCenter();
+    }
   });
 
   motionSensorFront.on('stop',function(){
-
+    motionLed.stopBlink();
   });
 
   motionSensorLeft.on('start',function(){
-
+    if(powerButton.isOn()){
+      motionLed.startBlink();
+      panLeft();
+    }
   });
 
   motionSensorLeft.on('stop',function(){
-
+    motionLed.stopBlink();
   });
 
   motionSensorRight.on('start',function(){
-
+    if(powerButton.isOn()){
+      motionLed.startBlink();
+      panRight();
+    }
   });
 
   motionSensorRight.on('stop',function(){
-
+    motionLed.stopBlink();
   });
+
+  function panLeft(){
+    baseServo.move(180);
+    camServo.move(85);
+  }
+
+  function panRight(){
+    baseServo.move(33);
+    camServo.move(85);
+  }
+
+  function panCenter(){
+    baseServo.move(110);
+    camServo.move(0);
+  }
+
+  //move camera to a rest position.
+  function sleepCam(){
+    baseServo.move(110);
+    camServo.move(0);
+  }
+
+  function executeCommand(cmd,cb){
+  	var child = cp.exec(cmd ,function(error,stdout,stderr){
+  		if (stderr){
+  			cb(false,"Error executing command "+cmd+" with stderr:"+stderr);
+  		}
+  		if (error) {
+  			cb(false,"Error executing command "+cmd+" with error:"+error);
+  		}
+  		if(stdout){
+  			cb(true,cmd+" successfully executed with no errors.",stdout);
+  		}
+  	});
+  }
+
 });
